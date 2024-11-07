c96facd3d0a453dc8bb6b3b1ab76ae210666f895
ewellyn <itsewellyn@gmail.com>
2024-11-07 17:24:32 -0300

    feat: added the button component

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -4,11 +4,13 @@ import Profile from '../components/Profile'
 import Story from '../components/Story'
 import Experiences from '../components/Experiences'
 import Contact from '../components/Contact'
+import Button from '../components/Button'
 
 const Portfolio = () => {
   return (
     <div className="font-roboto"> 
       <Header /> 
+      <Button />
       <div> 
         <Profile /> 
         <Story /> 
