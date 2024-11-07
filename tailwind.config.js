e07dbd388b5ddde7d04fb63a07dccdb77b373ec0
ewellyn <itsewellyn@gmail.com>
2024-11-07 15:45:13 -0300

    feat: initial stylization of experiences component

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,9 +18,18 @@ module.exports = {
       fontSize: {
         "0xl": "26.2px",
         "4xl": "38px",
+        "5xl": "42px",
       },
       fontFamily: {
         roboto: ["Roboto", "sans-serif"],
+      },
+      height: {
+        "38": "170px",
+        "100": "440px",
+      },
+      boxShadow: { 
+        '3xl': '2px 2px rgba(0, 0, 0, 0), 6px 6px rgba(0, 0, 0, 0)',
+        '4xl': '1px 1px 1px 3px rgba(0, 0, 0, 1), 4px 0 1px 0 rgba(0, 0, 0, 1)',
       },
     },
   },
