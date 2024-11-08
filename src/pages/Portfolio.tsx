fba1c700aae640461d3e549d2b23338001e2ba13
ewellyn <itsewellyn@gmail.com>
2024-11-08 16:15:19 -0300

    feat: added the git api

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,24 +1,38 @@
-import React from 'react'
-import Header from '../components/Header'
-import Profile from '../components/Profile'
-import Story from '../components/Story'
-import Experiences from '../components/Experiences'
-import Contact from '../components/Contact'
-import Button from '../components/Button'
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import Header from '../components/Header';
+import Profile from '../components/Profile';
+import Story from '../components/Story';
+import Experiences from '../components/Experiences';
+import Contact from '../components/Contact';
+import Button from '../components/Button';
 
-const Portfolio = () => {
+interface GitHubUser  {
+  login: string;
+  avatar_url: string;
+  html_url: string;
+  name: string;
+  email: string | null;
+  location: string | null;
+  bio: string | null;
+}
+
+const Portfolio: React.FC = () => {
+  const location = useLocation();
+  const userData = location.state?.userData as GitHubUser  | null;
+
   return (
     <div className="font-roboto"> 
       <Header /> 
       <Button />
       <div> 
-        <Profile /> 
+        {userData ? <Profile userData={userData} /> : <p>Carregando...</p>}
         <Story /> 
         <Experiences />
         <Contact />
       </div> 
     </div>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
