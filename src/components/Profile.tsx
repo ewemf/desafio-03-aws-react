c18e135d80c6e83269ea70562304b883d0c33629
ewellyn <itsewellyn@gmail.com>
2024-11-10 00:16:51 -0300

    feat: adding more of the logic, link modals

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import edit from '../imgs/edit-icon.svg';
+import LinkModal from '../components/LinkModal'; 
 
 interface GitHubUser {
   login: string;
@@ -19,7 +20,28 @@ interface ProfileProps {
 }
 
 const Profile: React.FC<ProfileProps> = ({ userData, isEditing, onEditChange }) => {
-  const [isEditingLinkedIn, setIsEditingLinkedIn] = useState(false);
+  const [linkedinUrl, setLinkedinUrl] = useState(userData.linkedin_url || "");
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const saveLink = (url: string) => {
+    setLinkedinUrl(url);
+    onEditChange('linkedin_url', url);
+  };
+
+  const handleExternalLink = (url: string) => {
+    if (!url.startsWith('http://') && !url.startsWith('https://')) {
+      url = 'http://' + url;
+    }
+    window.open(url, '_blank');
+  };
 
   return (
     <div className="flex justify-between my-32 mb-16 gap-x-24">
@@ -53,37 +75,40 @@ const Profile: React.FC<ProfileProps> = ({ userData, isEditing, onEditChange }) 
             <button className="bg-dark_green text-white text-xl font-semibold w-32 px-1 py-2 rounded-xl shadow-primary_color shadow-3xl hover:bg-primary_color">Github</button>
           </a>
           {isEditing ? (
-            isEditingLinkedIn ? (
-              <input
-                className="border-b-2 border-primary_color w-48"
-                type="text"
-                value={userData.linkedin_url}
-                onChange={(e) => onEditChange('linkedin_url', e.target.value)}
-                onBlur={() => setIsEditingLinkedIn(false)}
-              />
-            ) : (
-              <div className='relative inline-block'>
+            <>
+            <div className="relative inline-block cursor-pointer">
               <button 
                 className="bg-dark_green text-white text-xl font-semibold w-48 px-1 py-2 rounded-xl shadow-primary_color shadow-3xl hover:bg-primary_color"
-                onClick={() => setIsEditingLinkedIn(true)}
+                onClick={openModal}
               >
                 LinkedIn
-                
               </button>
-              <div className="absolute -top-1 -right-1 bg-card_color rounded-full w-4 h-4 flex items-center justify-center">
-                <img className='w-2' src={edit} alt="Edit Button" />
+              <div className="absolute bg-card_color rounded-full w-5 h-5 flex items-center justify-center -top-1 -right-1">
+                <img className='w-3' src={edit} alt="Edit Button" />
               </div>
-              </div>
-            )
+            </div>
+          </>
           ) : (
-            userData.linkedin_url && (
-              <a href={userData.linkedin_url} target="_blank" rel="noopener noreferrer">
-                <button className="bg-dark_green text-white text-xl font-semibold w-48 px-1 py-2 rounded-xl shadow-primary_color shadow-3xl hover:bg-primary_color">LinkedIn</button>
-              </a>
+            linkedinUrl && (
+              <button 
+                className="bg-dark_green text-white text-xl font-semibold w-48 px-1 py-2 rounded-xl shadow-primary_color shadow-3xl hover:bg-primary_color"
+                onClick={() => handleExternalLink(linkedinUrl)}
+              >
+                LinkedIn
+              </button>
             )
           )}
         </div>
       </div>
+
+      {isModalOpen && (
+        <LinkModal
+          isOpen={isModalOpen}
+          initialUrl={linkedinUrl}
+          onClose={closeModal}
+          onSave={saveLink}
+        />
+      )}
     </div>
   );
 };
